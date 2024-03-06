d55264035350ab7c5f2c12428b64ffb2874f9931
WaveextOnXX <145377312+WaveextOnXX@users.noreply.github.com>
2024-03-06 13:01:25 -0500

    finished problem one and two

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Transactions;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many collums ");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine(" how many rows");
+            int row = int.Parse(Console.ReadLine());
+
+            for (int currRow = 1; currRow <= row; currRow++)
+            {
+                for (int j = 1; j <= width; j++)
+
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+
+            }
+
+            Console.Write("how many *");
+            int maxAster = int.Parse(Console.ReadLine());
+
+            for (int r = 1; r <= maxAster; r++)
+            {
+
+                for (int c = 1; c <= r; c++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }       
+        }
+    }
+}
